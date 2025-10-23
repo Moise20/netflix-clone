531d9848e9c7816daa1b84efcd73a19ba33e5739
PANA <panaessognim@gmail.com>
2025-10-24 00:37:56 +0200

    fix: import NgIf and correct naming in Card and Row components

--- src/app/shared/ui/row/row.ts
+++ src/app/shared/ui/row/row.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { Card } from '../card/card';
 import { NgFor } from '@angular/common';
+import { Movie } from '../../../core/models/movie';
 
 @Component({
   selector: 'app-row',
@@ -10,5 +11,8 @@ import { NgFor } from '@angular/common';
   styleUrl: './row.css',
 })
 export class Row {
-
+@Input() title = '';
+  @Input() items: Movie[] = [];
+  @ViewChild('rail') rail!: ElementRef<HTMLDivElement>;
+  scroll(dir: 1|-1) { this.rail.nativeElement.scrollBy({left: dir*300, behavior:'smooth'}); }
 }
