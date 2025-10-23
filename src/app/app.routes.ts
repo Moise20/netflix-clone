1bbd075e427516a365bfdd80ae570da8aa35c957
PANA <panaessognim@gmail.com>
2025-10-23 22:27:49 +0200

    feat(router): routes lazy standalone (home, browse, details, search)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home').then(m => m.Home)
+  },
+  {
+    path: 'browse',
+    loadComponent: () =>
+      import('./features/browse/browse').then(m => m.Browse)
+  },
+  {
+    path: 'title/:id',
+    loadComponent: () =>
+      import('./features/details/details').then(m => m.Details)
+  },
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./features/search/search').then(m => m.Search)
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', redirectTo: 'home' },
+];
