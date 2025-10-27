2c25d7df9df34e7e85439902d0d829e6bb3b9464
PANA <panaessognim@gmail.com>
2025-10-27 10:54:41 +0100

    feat(browse): add browse page with category rows

--- src/app/features/browse/browse.ts
+++ src/app/features/browse/browse.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DataService } from '../../core/services/data.service';
+import { Navbar } from '../../shared/ui/navbar/navbar';
+import { NgFor, NgIf } from '@angular/common';
+import { Row } from '../../shared/ui/row/row';
 
 @Component({
   selector: 'app-browse',
-  imports: [],
+  imports: [Navbar, NgFor, NgIf, Row],
   templateUrl: './browse.html',
   styleUrl: './browse.css',
 })
 export class Browse {
+private data = inject(DataService);
+  rows: any[] = [];
 
+  ngOnInit() {
+    this.data.getHome().subscribe(res => {
+      this.rows = res.rows; // ✅ affiche toutes les catégories
+    });
+  }
 }
